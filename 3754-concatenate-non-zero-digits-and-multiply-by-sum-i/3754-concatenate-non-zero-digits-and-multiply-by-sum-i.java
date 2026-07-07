7cc6a0678a9262c02575e776721a458b38c8e0cf
Aditya Rawat <rrawataditya2003@gmail.com>
2026-07-07 15:13:15 +0530

    Time: 1 ms (99.85%), Space: 42.7 MB (52.17%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -4,12 +4,11 @@ class Solution {
         long sum=0,digit=0,i=1;
         while(n>0){
             long a=n%10;
-           if(a!=0){
+            n/=10;
+           if(a==0)continue;
            sum+=a;
            digit+=i*a;
            i*=10;
-           }
-           n/=10;
         }
         return sum*digit;
         
