84b465619fe48b3a50752d65513f5b63fba7b0a2
Aditya Rawat <rrawataditya2003@gmail.com>
2026-08-16 23:22:46 +0530

    Time: 8 ms (12.84%), Space: 114.8 MB (64.86%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int a=0,b=0,c=0;
+        for(int num:stones){
+            int rem=num%3;
+            if(rem==0)c=c+1;
+            if(rem==1)a=a+1;
+            if(rem==2)b=b+1;
+        }
+        if(c%2==0)return a>0&&b>0;
+
+        return Math.abs(a-b)>2; 
+    }
+}
